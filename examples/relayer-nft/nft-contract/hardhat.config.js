005e7bd5ea44a22fb86827e964976de8fd9c4331
drumfeet <drumfeet@gmail.com>
2023-07-17 15:28:04 +0800

    code refactor

--- examples/relayer-nft/nft-contract/hardhat.config.js
+++ examples/relayer-nft/nft-contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     mumbai: {
-      url: process.env.EVM_RPC,
+      url: process.env.EVM_RPC_URL,
       accounts: [process.env.PRIVATEKEY],
     },
   },
